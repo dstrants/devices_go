0eb237486b1c55003b86a015e9f28f7cc9b327f3
Dimitris <dimitris.strantsalis@workable.com>
2022-01-26 12:14:46 +0200

    ✨  Port all logging to logrus

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 
@@ -15,6 +16,14 @@ const Version = "0.1.1"
 
 var cnf config.Config = config.LoadConfig()
 
+// Removes line endings from user input field
+func escapeInputField(field string) string {
+	escapedField := strings.Replace(field, "\n", "", -1)
+	escapedField = strings.Replace(escapedField, "\r", "", -1)
+
+	return escapedField
+}
+
 func Authenticator() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		headers := c.Request.Header
@@ -22,6 +31,7 @@ func Authenticator() gin.HandlerFunc {
 
 		// If there is not Token header
 		if !ok {
+			log.Error("Request did not include a token header")
 			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{"message": "UnprocessableEntity"})
 		}
 
@@ -31,11 +41,13 @@ func Authenticator() gin.HandlerFunc {
 		}
 
 		// If the token exists bit it is wrong
+		log.Error("Authentication failed")
 		c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"message": "Access Denied"})
 	}
 }
 
 func main() {
+
 	r := setupRouter()
 	r.Run()
 }
@@ -49,15 +61,21 @@ func setupRouter() *gin.Engine {
 	r.POST("devices/", func(c *gin.Context) {
 		var device devices.Device
 		if err := c.ShouldBindJSON(&device); err != nil {
+			log.Error("Could not parse request body")
 			c.JSON(http.StatusUnprocessableEntity, gin.H{"message": "Could not parse device status"})
 		}
 
 		// Send slack notification and save the data to mongo
-		device.Notify()
-		err := device.AutoTimestamp().Save()
+		err := device.Notify()
 
 		if err != nil {
-			log.Fatal(err)
+			log.WithError(err).Error("An error occurred while sending notification")
+		}
+
+		err = device.AutoTimestamp().Save()
+
+		if err != nil {
+			log.WithError(err).WithFields(log.Fields{"device": escapeInputField(device.Name)}).Error("An error occurred while saving event to mongo")
 		}
 
 		c.JSON(http.StatusAccepted, gin.H{"device": device.Name, "status": device.EventMessage()})
